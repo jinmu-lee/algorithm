4a07652bb98aa7cb5732267da23c366bb29c5dd6
jinmu.lee <jinmu.lee@lge.com>
2020-05-12 10:20:52 +0900

    complex example

--- 05_cpp_stl_programming/STL-master/1020_PREVIEW/complex1.cpp
+++ 05_cpp_stl_programming/STL-master/1020_PREVIEW/complex1.cpp
@@ -8,6 +8,18 @@ using std::endl;
 using namespace std::literals;
 //using namespace std;
 
+#if 0
+
+Some functions supported in a header of complex
+
+Exponential : exp,log,log10
+Power : pow, sqrt
+Trigometric : sin, cos, tan, asin, acos, atan
+Hyperbolic : sinh, cosh, tanh, asinh, acosh, atanh
+refer to : cppreference.com
+
+#endif
+
 int main()
 {
 	complex<double> c1(1, 0);
@@ -19,11 +31,18 @@ int main()
 	complex<double> c2 = sin(c1);
 	cout << c2 << endl;
 
-//	complex<int> c3(1, 0);
-//	complex<int> c4 = sin(c3);
+#if 0
+	complex<int> c3(1, 0); // complex type only support float / double
+	complex<int> c4 = sin(c3); // Unexpected
+#endif
 
 	complex<float> c5(3); // 3, 0
 	complex<float> c6(3, 1); // 3, 1
+#if 0
+	3.2f -> complex<double>
+	3.2if -> complex<float>
+	3.2lf -> complex<long double>
+#endif
 	complex<float> c7(3if); // 0, 3
 
 	cout << c5 << endl;
